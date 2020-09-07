085f7db4237d67318d982711d3af063aa3c5d1fb
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-09-07 14:35:39 +0200

    New version v3.9-r1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opencast';
-$plugin->release   = 'v3.8-r1'; // First release under Moodle 3.8.
-$plugin->version = 2019112100;
+$plugin->release   = 'v3.9-r1'; // First release under Moodle 3.8.
+$plugin->version = 2020090700;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
