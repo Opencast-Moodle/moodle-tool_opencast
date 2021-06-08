2a0cdae8178a3931c1416793eadf822233b99580
Tamara Gunkel <tamara.gunkel@web.de>
2021-06-08 21:32:16 +0200

    rename default field in install

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opencast';
 $plugin->release   = 'v3.11-r2'; // First release under Moodle 3.11.
-$plugin->version = 2021060400;
+$plugin->version = 2021060800;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
