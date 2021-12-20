0b2de8a71dc6af99fdf057e41a847dfc23282e66
TamaraGunkel <tamara.nrw@web.de>
2021-12-20 12:54:59 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opencast';
-$plugin->release   = 'v3.11-r5'; // Fifth release under Moodle 3.11.
-$plugin->version = 2021120100;
+$plugin->release   = 'v3.11-r6'; // Fifth release under Moodle 3.11.
+$plugin->version = 2021122000;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
