fe2d5d7107aac4884022d61e83e6c4b441b87152
Tamara Gunkel <tamara.gunkel@web.de>
2022-03-08 20:59:41 +0100

    update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opencast';
-$plugin->release   = 'v3.11-r7'; // Fifth release under Moodle 3.11.
-$plugin->version = 2022021001;
+$plugin->release   = 'v3.11-r7'; // Seventh release under Moodle 3.11.
+$plugin->version = 2022030802;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
