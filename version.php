83ada6d1c7a0b57246fb00162dd5ca4f3ebb3928
Matthias Kollenbroich <matthias.kollenbroich@uni-muenster.de>
2023-02-23 12:29:57 +0100

    Updated version information for v4.0-r1

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_opencast';
 $plugin->release   = 'v4.0-r1';
-$plugin->version = 2022111900;
+$plugin->version = 2023030100;
 $plugin->requires = 2020061500; // Requires Moodle 3.9+.
 $plugin->maturity = MATURITY_STABLE;
