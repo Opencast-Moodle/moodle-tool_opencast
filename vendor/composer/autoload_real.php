cd9a3340f6341f86e1288e421264fbca58bbdf82
tamaro <tamaro.walter@uni-muenster.de>
2026-09-26 12:40:28 +0200

    Stop plugin autoloader from overriding Moodle core's Guzzle
    The prepended Composer autoloader mixed core's and the plugin's
    guzzlehttp/psr7 classes in one request, causing
    "Call to undefined method GuzzleHttp\Psr7\Uri::assertValidHost()"

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,14 +22,14 @@ class ComposerAutoloaderInit8ba2ca4e105ea351f7cb241590a7de7e
             return self::$loader;
         }
 
-        spl_autoload_register(array('ComposerAutoloaderInit8ba2ca4e105ea351f7cb241590a7de7e', 'loadClassLoader'), true, true);
+        spl_autoload_register(array('ComposerAutoloaderInit8ba2ca4e105ea351f7cb241590a7de7e', 'loadClassLoader'), true, false);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit8ba2ca4e105ea351f7cb241590a7de7e', 'loadClassLoader'));
 
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInit8ba2ca4e105ea351f7cb241590a7de7e::getInitializer($loader));
 
-        $loader->register(true);
+        $loader->register(false);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit8ba2ca4e105ea351f7cb241590a7de7e::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
